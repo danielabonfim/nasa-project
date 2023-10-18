bfaf3b370a0ef1b5e34ba48487d991d223ddb8bc
Dani Bonfim <daniela@superopa.com>
2023-10-17 21:47:41 -0300

    add: morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
-//middlewares
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 
-//serving static files
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-const path1 = path.join(__dirname, '..', 'public', 'index.html')
 app.get('/', (req, res) => {
-  console.log("path : ", path1)
-  console.log("oi")
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
