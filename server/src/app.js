2cb55625e4605a70b76d9d7ec8e42db22d826a5d
Dani Bonfim <daniela@superopa.com>
2023-10-17 21:39:33 -0300

    add: deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +11,17 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+
+//serving static files
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const path1 = path.join(__dirname, '..', 'public', 'index.html')
+app.get('/', (req, res) => {
+  console.log("path : ", path1)
+  console.log("oi")
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
+
 app.use(planetsRouter);
 
 module.exports = app;
